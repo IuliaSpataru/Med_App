aa9de23bd0fbb761788d661bc89ecc72443b41b4
IuliaSpataru <iulia.spataru@gmail.com>
2025-01-30 19:46:44 +0200

    1st commit

--- synccare/settings.py
+++ synccare/settings.py
@@ -123,11 +123,16 @@ USE_TZ = True
 # Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'synccare' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# test
